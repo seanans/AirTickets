e502e6d4352724d0dc8355b0f4dca145349f7aaa
seanans <maks1998ivan@gmail.com>
2023-08-11 16:27:08 +0300

    init commit

---
+++ src/test/java/com/seanans/AirTickets/AirTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanans.AirTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
